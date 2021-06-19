ed1b683cd4b8b12be24d24d5a9bb995e272017f7
drdr xp <drdr.xp@gmail.com>
2021-06-19 21:10:47 +0800

    M  applytmpl.sh

--- applytmpl.sh
+++ applytmpl.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 
+cp ../tmpl/_building/Makefile                  ./_building/
+cp ../tmpl/_building/README.md                 ./_building/
+cp ../tmpl/_building/README.md.j2              ./_building/
 cp ../tmpl/_building/__init__.py               ./_building/
 cp ../tmpl/_building/build_readme.py           ./_building/
 cp ../tmpl/_building/build_setup.py            ./_building/
 cp ../tmpl/_building/building-requirements.txt ./_building/
 cp ../tmpl/_building/common.mk                 ./_building/
-cp ../tmpl/_building/Makefile                  ./_building/
-cp ../tmpl/_building/README.md                 ./_building/
-cp ../tmpl/_building/README.md.j2              ./_building/
+cp ../tmpl/_building/install.sh                ./_building/
 cp ../tmpl/_building/requirements.txt          ./_building/
 
 cp ../tmpl/docs/source/conf.py                 ./docs/source/
