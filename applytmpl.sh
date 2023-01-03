a71be354a0d7bfc3aa1d5f22a907845947e40829
张炎泼 <drdr.xp@gmail.com>
2023-01-03 21:20:24 +0800

    applytmpl should copy github/workflows/*

--- applytmpl.sh
+++ applytmpl.sh
@@ -13,3 +13,6 @@ cp ../tmpl/_building/install.sh                ./_building/
 cp ../tmpl/_building/requirements.txt          ./_building/
 
 cp ../tmpl/docs/source/conf.py                 ./docs/source/
+
+cp ../tmpl/.github/workflows/python-package.yml .github/workflows/
+cp ../tmpl/.github/workflows/python-publish.yml .github/workflows/
